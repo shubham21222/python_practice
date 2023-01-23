3cd11c2de8b5cb20f722d52659cd8a2b4258d8bc
shubham raikwar <119775775+shubham21222@users.noreply.github.com>
2023-01-23 14:57:41 +0530

    Add files via upload

---
+++ 2.6.1 dromy.py
@@ -0,0 +1,7 @@
+var1 = int(input("enter first fibonacci number: "))
+var2 = int(input("enter second fibonacci number: "))
+if var2<var1 :
+    print("value 2 must be greater then value 1")
+else:
+    var3 = var1+var2
+    print("third fibonacci number should be: ",var3)
