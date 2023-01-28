597a49c9c0eab55279bc5b4e53d6c677a956e469
shubham raikwar <119775775+shubham21222@users.noreply.github.com>
2023-01-28 14:13:58 +0530

    Update 3.2.2 dromy.py

---
+++ 3.2.2 dromy.py
@@ -0,0 +1,9 @@
+divisor=int(input("enter a number"))
+x= list(range(1,divisor+1))
+divisorlist=[]
+
+for number in x:
+    if divisor % number == 0:
+      divisorlist.append(number)
+    print(divisorlist)
+
