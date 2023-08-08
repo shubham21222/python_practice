b092a0bc809d184ffba9a75ed73f7a6d9031869b
shubham raikwar <119775775+shubham21222@users.noreply.github.com>
2023-08-08 09:39:19 +0000

    practice list question

---
+++ list_question.py
@@ -0,0 +1,8 @@
+#qus:- write a pyton program whose taking a list and check which number is greater than given number 
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b=[]
+for i in a:
+    if i< 5:
+        b.append(i)
+        print(b)
