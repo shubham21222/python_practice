752855efe0e68f194adac29acd61e0a41d1eee5f
shubham raikwar <119775775+shubham21222@users.noreply.github.com>
2023-08-08 08:23:23 +0000

    do-me loop implemention in python

--- demo3.py
+++ demo3.py
@@ -1,3 +1,5 @@
+# this code demonstrates how to implement a "do-while" loop in Python using functions.
+
 def dowhile(func = None, condition = None):
     if not func or not condition:
         return
