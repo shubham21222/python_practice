b5365ae36ebb27194128312e6b8c94a385da69e1
shubham raikwar <119775775+shubham21222@users.noreply.github.com>
2023-08-08 08:05:04 +0000

    addition and multipication program

--- addition.py
+++ addition.py
@@ -1,11 +1,9 @@
-'''num1=int(input("enter first number"))
+#qus:- write a  basic python program for addition & multipcation of two numbers
+
+num1=int(input("enter first number"))
 num2=int(input("enter second number"))
 num3=int(input("enter third number"))
 addition=num1+num2
 multipiction=addition*num3
-print("the answer is:",multipiction)'''
+print("the answer is:",multipiction)
 
-bill=float(input("enter total bill"))
-total=int(input("total diners"))
-eachpay=bill/total
-print("each person pay",eachpay)
