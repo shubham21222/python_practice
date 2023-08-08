564062da28811440bfcdaf877a8e4bbbb761c450
shubham raikwar <119775775+shubham21222@users.noreply.github.com>
2023-08-08 08:59:49 +0000

    calculate mean of given numbers

---
+++ mean_of_4_numbers.py
@@ -0,0 +1,8 @@
+#qus:- write a python program which takes a input from user and calculate its mean
+multiply_values = int(input("enter value 1: "))
+multiply_values2 = int(input("enter value 2 :"))
+multiply_values3 = int(input("enter value 3 :"))
+multiply_values4 = int(input("enter value 4 : "))
+n = int(input("enter the value of n: "))
+geometric_mean = (multiply_values*multiply_values2*multiply_values3*multiply_values4)**(1/n)
+print ('The Geometric Mean is: ' ,(geometric_mean))
