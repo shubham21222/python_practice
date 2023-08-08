3d353acd5c0c3789601c28a0ca481203596360dd
shubham raikwar <119775775+shubham21222@users.noreply.github.com>
2023-08-08 07:58:08 +0000

    camel case pyton program

---
+++ camel_case.py
@@ -0,0 +1,18 @@
+#qus:- write a python program for camel case.
+
+#take a variable and alocate a user define string in it.
+#now take another variable and alocate  and value to it.
+#repeat the below step for 9 times
+#take ith index character if i(!=1):
+
+
+value1=input("enter the string")
+value2=''
+for i in range (len(value1)):
+    if i !=1:
+        value2=value2+value1[i].lower()
+    else:
+        value2=value2+value1[i].upper()
+    print("user given value=",value1)
+    print("camel case of given value=",value2)           
+    
