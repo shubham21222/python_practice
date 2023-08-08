ee291cf14260955ff54765109c36bfe2133ff285
shubham raikwar <119775775+shubham21222@users.noreply.github.com>
2023-08-08 08:08:06 +0000

    change string in upparcase and lowercase latters

--- assignment.py
+++ assignment.py
@@ -1,11 +1,21 @@
-input1=input("enter your string")
-x=len(input1)
-shubham = input()
-for i in range (x):
-    if shubham[i]>='a' and shubham[i]<='z':
-        s = x[i]
-        s = ord(s)
-        s = s-32
-        s = chr(s)
-        x = x[::1] + s + x[i+1:] 
-print ("uppercase :" , x)        
+#qus:- write a python program which take string as a input and change latters of string into uppercase latters.
+input1 = input("Enter your string: ")
+input2 = input("Enter another string with lowercase letters: ")
+
+
+if len(input2) < len(input1):
+    print("Error: The second string should be at least as long as the first string.")
+else:
+    result = ""
+
+    for i in range(len(input1)):
+        if input2[i] >= 'a' and input2[i] <= 'z':
+            s = input1[i]
+            s = ord(s)
+            s = s - 32
+            s = chr(s)
+            result += s
+        else:
+            result += input1[i]
+
+    print("Modified string:", result)
