fc4617e14190e808e5ad24da443f880639472c0b
shubham raikwar <119775775+shubham21222@users.noreply.github.com>
2023-08-08 09:47:51 +0000

    list question

---
+++ list_1.py
@@ -0,0 +1,5 @@
+#qus:- write a python program which takes a list and print even number list
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+for num in a:
+    if num % 2 ==0:
+        print(num,end=" ")
