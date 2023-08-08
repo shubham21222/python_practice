ea8bdc6866d21bf4c55fc2800a0e06e77d174378
shubham raikwar <119775775+shubham21222@users.noreply.github.com>
2023-08-08 08:41:31 +0000

    binary search

---
+++ binary_search.py
@@ -0,0 +1,14 @@
+#qus:- binary search to find the first occurrence of a specific value within a sorted list
+
+a=[10,20,30,11,12,13,14,15,16,17,19,20,22,24,32]
+lower=1
+upper=15
+while lower<upper :
+    middle=(lower+upper)/2
+if 15>a:
+    lower=middle+1
+else:
+    upper=middle
+print(middle)  
+
+
